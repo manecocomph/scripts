bf39fd6d54a196884b0361b8dbe5d27908d9538f
Eric <manecocomph@163.com>
2022-01-10 03:13:17 -0700

    Update dropdown.js

--- emptyScript/jQuery/dropdown.js
+++ emptyScript/jQuery/dropdown.js
@@ -1,64 +1,94 @@
 
+//// below are utils
 jQuery.fn.extend({
-    refreshSuggest: function() {
-        let options = $(this).attr("options");
+    refreshSuggest: function(suggestLength = 10, delayMs = 10000) {
+        
         let curVal = $(this).val();
-        if (options && curVal) {
-            let optionsHtml = "";
-            let optionList = options.split(",");
-            let matchedCount = 0;
-            for (let option of optionList) {
-                if (option.indexOf(curVal) > -1) {
-                    optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' readonly/></li>`;
-                    if (matchedCount++ > 10) {
-                        break;
-                    }
+        let suggestDiv = this.prev("div[name='_suggest']");
+        if (curVal && suggestDiv.length > 0) {
+            let options = suggestDiv.attr("options");
+            if (options) {
+                let optionsHtml = "";
+                let optionList = options.split(",");
+                let matchedCount = 1;
+                if (' ' === curVal) {// if blank, then show some without filter
+                    for (let option of optionList) {
+                        optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' readonly/></li>`;
+                        if (++matchedCount > suggestLength) {
+                            break;
+                        }
+                    };
+                } else {
+                    for (let option of optionList) {
+                        if (option.indexOf(curVal) > -1) {
+                            optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' readonly/></li>`;
+                            if (++matchedCount > suggestLength) {
+                                break;
+                            }
+                        }
+                    };
                 }
-            };
-            let suggestDiv = this.prev("div[name='_suggest']");
-            suggestDiv.find("ol[name='_suggest']").html(optionsHtml);
+                
+                suggestDiv.find("ol[name='_suggest']").html(optionsHtml);
 
-            suggestDiv.find("input[name='_suggest']").keyup(event => {
-                const key = event.keyCode;
-                let cur = $(event.target);
-                if (40 === key) {//down array
-                    cur.css("backgroundColor", "");
-                    let next = cur.parent().next().find("input");
-                    if (0 === next.length) {
-                        next = cur.parent().parent().find("input").first();
+                function delayFadeout() {//auto fadeout when no action on suggest div
+                    let prevTimeoutId = suggestDiv.attr("timeoutId");
+                    if (prevTimeoutId) {
+                        clearTimeout(prevTimeoutId);
                     }
-                    next.css('backgroundColor', 'lightgray');
-                    next.focus();
-                } else if (38 === key) {//up array
-                    cur.css("backgroundColor", "");
-                    let prev = cur.parent().prev().find("input");
-                    if (0 === prev.length) {
-                        prev = cur.parent().parent().find("input").last();
+                    let timeoutId = setTimeout(function(){
+                        suggestDiv.find("ol").html("");
+                    }, delayMs);
+                    suggestDiv.attr("timeoutId", timeoutId);
+                };
+                delayFadeout();
+
+                // keyup actions
+                suggestDiv.find("input[name='_suggest']").keyup(event => {
+                    const key = event.keyCode;
+                    let cur = $(event.target);
+                    if (40 === key) {//down array
+                        cur.css("backgroundColor", "");
+                        let next = cur.parent().next().find("input");
+                        if (0 === next.length) {
+                            next = cur.parent().parent().find("input").first();
+                        }
+                        next.css('backgroundColor', 'lightgray');
+                        next.focus();
+                        delayFadeout();
+                    } else if (38 === key) {//up array
+                        cur.css("backgroundColor", "");
+                        let prev = cur.parent().prev().find("input");
+                        if (0 === prev.length) {
+                            prev = cur.parent().parent().find("input").last();
+                        }
+                        prev.css('backgroundColor', 'lightgray');
+                        prev.focus();
+                        delayFadeout();
+                    } else if (13 === key) {//carriage ret
+                        $(this).val(cur.val());
+                        suggestDiv.find("ol").html("");
+                        $(this).focus();
                     }
-                    prev.css('backgroundColor', 'lightgray');
-                    prev.focus();
-                } else if (13 === key) {//carriage ret
+                });
+
+                // click action
+                suggestDiv.find("input[name='_suggest']").click(event => {
+                    let cur = $(event.target);
                     $(this).val(cur.val());
                     suggestDiv.find("ol").html("");
                     $(this).focus();
-                }
-
-            });
-
-            suggestDiv.find("input[name='_suggest']").click(event => {
-                let cur = $(event.target);
-                $(this).val(cur.val());
-                suggestDiv.find("ol").html("");
-                $(this).focus();
-            });
+                });  
+            }
         }
     },
-    addSuggest: function() {
+    addSuggest: function(options, suggestLength = 10) {
         let suggestDiv = this.prev("div[name='_suggest']");
         if (!suggestDiv || "_suggest" !== $(suggestDiv).attr("name")) {
             $("<div name='_suggest' style='position:relative;display:inline-block;'><ol name='_suggest' style='z-index:100;position:absolute;top:1em;padding-left:1em;'></ol></div>" ).insertBefore(this);
             suggestDiv = this.prev("div[name='_suggest']");
         }
+        suggestDiv.attr("options", options);
 
         this.keyup(event => {
             const key = event.keyCode;
@@ -91,10 +121,10 @@ jQuery.fn.extend({
                     last.focus();
                 }
             } else {
-                this.refreshSuggest();
+                this.refreshSuggest(suggestLength);
             }
         });
-        this.refreshSuggest();
+        this.refreshSuggest(suggestLength);
     }
 });
 
