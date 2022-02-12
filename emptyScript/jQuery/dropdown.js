232900e81b71163ff23de8344059641b78c87290
Eric <manecocomph@163.com>
2022-02-11 22:38:33 -0700

    Update dropdown.js

--- emptyScript/jQuery/dropdown.js
+++ emptyScript/jQuery/dropdown.js
@@ -1,27 +1,28 @@
 
 //// below are utils
 jQuery.fn.extend({
-    refreshSuggest: function(suggestLength = 10, delayMs = 10000) {
-        
+    refreshSuggest: function(selectedCallback, suggestLength = 10, delayMs = 3000) {
         let curVal = $(this).val();
         let suggestDiv = this.prev("div[name='_suggest']");
         if (curVal && suggestDiv.length > 0) {
+            curVal = curVal.toLowerCase();
             let options = suggestDiv.attr("options");
             if (options) {
+                let width = Math.round($(this).width());
                 let optionsHtml = "";
                 let optionList = options.split(",");
                 let matchedCount = 1;
                 if (' ' === curVal) {// if blank, then show some without filter
                     for (let option of optionList) {
-                        optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' readonly/></li>`;
+                        optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' style='width:${width}px' readonly/></li>`;
                         if (++matchedCount > suggestLength) {
                             break;
                         }
                     };
                 } else {
                     for (let option of optionList) {
-                        if (option.indexOf(curVal) > -1) {
-                            optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' readonly/></li>`;
+                        if (option.toLowerCase().indexOf(curVal) > -1) {
+                            optionsHtml += `<li><input name='_suggest' val='${option}' value='${option}' style='width:${width}px' readonly/></li>`;
                             if (++matchedCount > suggestLength) {
                                 break;
                             }
@@ -69,6 +70,9 @@ jQuery.fn.extend({
                         $(this).val(cur.val());
                         suggestDiv.find("ol").html("");
                         $(this).focus();
+                        if (selectedCallback) {
+                            selectedCallback();
+                        }
                     }
                 });
 
@@ -78,53 +82,67 @@ jQuery.fn.extend({
                     $(this).val(cur.val());
                     suggestDiv.find("ol").html("");
                     $(this).focus();
+                    if (selectedCallback) {
+                        selectedCallback();
+                    }
                 });  
             }
         }
     },
-    addSuggest: function(options, suggestLength = 10) {
+    addSuggest: function(options, selectedCallback, suggestLength = 10, delayMs = 3000) {
         let suggestDiv = this.prev("div[name='_suggest']");
-        if (!suggestDiv || "_suggest" !== $(suggestDiv).attr("name")) {
-            $("<div name='_suggest' style='position:relative;display:inline-block;'><ol name='_suggest' style='z-index:100;position:absolute;top:1em;padding-left:1em;'></ol></div>" ).insertBefore(this);
-            suggestDiv = this.prev("div[name='_suggest']");
+        if (suggestDiv.length < 1) {
+            suggestDiv = $("<div name='_suggest' style='position:relative;display:inline-block;'><ol name='_suggest' style='z-index:100;position:absolute;top:1em;padding-left:1em;'></ol></div>" );
+            suggestDiv.insertBefore(this);
+
+            this.keyup(event => {
+                const key = event.keyCode;
+                let suggestDiv = this.prev("div[name='_suggest']");
+                if (13 === key) {//carriage ret
+                    if (suggestDiv.find("input").length < 1) {
+                        return;
+                    }
+                    let first = suggestDiv.find("input");
+                    if (first) {
+                        this.val(first.val());
+                        if (selectedCallback) {
+                            selectedCallback();
+                        }
+                    }
+                    suggestDiv.find("ol").html("");
+                } else if (40 === key) {// down array
+                    if (suggestDiv.find("input").length < 1) {
+                        return;
+                    }
+                    let first = suggestDiv.find("input").first();
+                    if (first) {
+                        first.css('backgroundColor', 'lightgray');
+                        first.focus();
+                    }
+                } else if (38 === key) {// up array
+                    if (suggestDiv.find("input").length < 1) {
+                        return;
+                    }
+                    let last = suggestDiv.find("input").last();
+                    if (last) {
+                        last.css('backgroundColor', 'lightgray');
+                        last.focus();
+                    }
+                } else {
+                    this.refreshSuggest(selectedCallback, suggestLength, delayMs);
+                }
+            });
         }
         suggestDiv.attr("options", options);
 
-        this.keyup(event => {
-            const key = event.keyCode;
-            let suggestDiv = this.prev("div[name='_suggest']");
-            if (13 === key) {//carriage ret
-                if (suggestDiv.find("input").length < 1) {
-                    return;
-                }
-                let first = suggestDiv.find("input");
-                if (first) {
-                    this.val(first.val());
-                }
-                suggestDiv.find("ol").html("");
-            } else if (40 === key) {// down array
-                if (suggestDiv.find("input").length < 1) {
-                    return;
-                }
-                let first = suggestDiv.find("input").first();
-                if (first) {
-                    first.css('backgroundColor', 'lightgray');
-                    first.focus();
-                }
-            } else if (38 === key) {// up array
-                if (suggestDiv.find("input").length < 1) {
-                    return;
-                }
-                let last = suggestDiv.find("input").last();
-                if (last) {
-                    last.css('backgroundColor', 'lightgray');
-                    last.focus();
-                }
-            } else {
-                this.refreshSuggest(suggestLength);
-            }
-        });
-        this.refreshSuggest(suggestLength);
+        this.refreshSuggest(selectedCallback, suggestLength, delayMs);
+    }, 
+    updateOptions(options, selectedCallback, suggestLength = 10, delayMs = 3000) {
+        let suggestDiv = this.prev("div[name='_suggest']");
+        if(suggestDiv.length > 0) {
+            suggestDiv.attr("options", options);
+        } else {
+            addSuggest(options, selectedCallback, suggestLength, delayMs);
+        }
     }
 });
-
